5344d803007e9c5381439fa7c34278c1ad24a367
Biswanath Das <cv.biswanath@gmail.com>
2022-11-02 15:49:00 +0530

    ok

--- MockProduct.js
+++ MockProduct.js
@@ -35,4 +35,86 @@ const products = [
     price: 99,
     props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
   },
+  {
+    id: 5,
+    title: "Darmi cool power",
+    image: "asset/blue.jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 99,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 6,
+    title: "Leap bum",
+    image: "asset/61GFTBViu0L._SL1180_.jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 199,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 7,
+    title: "Datol",
+    image: "asset/1l-dettol-antiseptic-liquid-500x500.png",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 199,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 8,
+    title: "Amul Milk",
+    image:
+      "asset/amul-taaza-toned-milk-500-ml-tetra-pak-product-images-o491022104-p491022104-0-202203142125.jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 199,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 9,
+    title: "Lays",
+    image: "asset/lays.jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 20,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 10,
+    title: "Red Wine",
+    image: "asset/51EpSA4JnuL._SL1100_.jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 2000,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 11,
+    title: "Tropicana red juice",
+    image: "asset/tropicana1 (1).jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 200,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 12,
+    title: "Tropicana mango juice",
+    image: "asset/tropicana1 (2).jpg",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 200,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
+  {
+    id: 13,
+    title: "Real mango juice",
+    image: "asset/tropicana1 (1).png",
+    description:
+      "Streax Hair Serum is vitalized with Walnut Oil to give your hair the instant shine and smoothness which helps you Shine All Day! Streax hair serum is crafted",
+    price: 200,
+    props: "color: red  |  bita-caratin: 100mg  |  oil: 10ml",
+  },
 ];
